b20eecc311f7477f9784ab808c3c8da2f0962d14
Kacper_Pietrusiak <kacperpietrusiak.biznes@gmail.com>
2023-10-26 16:40:27 +0200

    first commit

---
+++ src/sections/index.js
@@ -0,0 +1,10 @@
+import Hero from "./Hero.jsx";
+import Footer from "./Footer.jsx";
+import CustomerReviews from "./CustomerReviews.jsx";
+import PopularProducts from "./PopularProducts.jsx";
+import Services from "./Services.jsx";
+import SpecialOffer from "./SpecialOffer.jsx";
+import Subscribe from "./Subscribe.jsx";
+import SuperQuality from "./SuperQuality.jsx";
+
+export {Hero, Footer, Subscribe, Services, SuperQuality, PopularProducts, SpecialOffer, CustomerReviews}
